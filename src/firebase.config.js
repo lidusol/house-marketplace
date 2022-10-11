0de2be440faa5473bccbc3e5016d576396eb4087
Lydia Solomon <lydiasolomon97@gmail.com>
2022-10-12 00:39:14 +0300

    Add user sign up

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFireStore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDlG6S3y4MSNLekVCDoy0voTeUyXiaElxg",
@@ -12,4 +12,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFireStore();
+export const db = getFirestore();
