33a8aea3bfec8f8285e033e6144b23d5f8e49334
Lydia Solomon <lydiasolomon97@gmail.com>
2022-10-11 15:50:53 +0300

    Init project.

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlG6S3y4MSNLekVCDoy0voTeUyXiaElxg",
+  authDomain: "house-marketplace-app-9182a.firebaseapp.com",
+  projectId: "house-marketplace-app-9182a",
+  storageBucket: "house-marketplace-app-9182a.appspot.com",
+  messagingSenderId: "923798472793",
+  appId: "1:923798472793:web:98500667afdcd503ec4948",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFireStore();
