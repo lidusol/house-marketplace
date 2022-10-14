b985d645175e2a995aac65c3abca1240deddd821
Lydia Solomon <lydiasolomon97@gmail.com>
2022-10-14 16:07:55 +0300

    Remove console warnings.

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -14,6 +14,7 @@ import { toast } from "react-toastify";
 import { v4 as uuidv4 } from "uuid";
 
 function CreateListing() {
+  // eslint-disable-next-line
   const [geolocationEnabled, setGeolocationEnabled] = useState(false);
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
